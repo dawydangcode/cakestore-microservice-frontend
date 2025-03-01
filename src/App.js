8ec34df81e46ce860ba705b5c6339a10ef8b663f
dawydangcode <122167896+dawydangcode@users.noreply.github.com>
2025-03-01 09:59:05 +0700

    config JWT

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+// import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/layout/Navbar";
-// import Footer from "./components/layout/Footer";
 import Home from "./pages/home/Home";
 import ProductList from "./components/products/ProductList";
 import Cart from "./pages/CartPage";
+// import Signup from "./pages/SignUp";
+// import Signin from "./pages/SignIn";
 import "./App.css";
 
 function App() {
     return (
-        <CartProvider>
-            <Router>
-                <div className="App">
-                    <Navbar />
-                    <main className="main-content">
-                        <Routes>
-                            {/* Khi vào "/", sẽ hiển thị Home.js */}
-                            <Route path="/" element={<Home />} />
-                            <Route path="/products" element={<ProductList />} />
-                            <Route path="/cart" element={<Cart />} />
-                        </Routes>
-                    </main>
-                    {/* <Footer /> */}
-                </div>
-            </Router>
-        </CartProvider>
+            <CartProvider>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <main className="main-content">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/products" element={<ProductList />} />
+                                <Route path="/cart" element={<Cart />} />
+                                {/* <Route path="/signup" element={<Signup />} /> */}
+                            </Routes>
+                        </main>
+                    </div>
+                </Router>
+            </CartProvider>
     );
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
+// import { AuthContext } from "../../context/AuthContext";
 import "./Navbar.css";
 
 const Navbar = () => {
     const { cart } = useContext(CartContext);
+    // const { user, logout } = useContext(AuthContext);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <nav className="navbar">
@@ -17,6 +20,22 @@ const Navbar = () => {
                         {cart.length > 0 && <span className="cart-count">{cart.reduce((total, item) => total + item.quantity, 0)}</span>}
                     </Link>
                 </li>
+                <li className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>☰</li>
+                {/* {menuOpen && (
+                    <div className="dropdown-menu">
+                        {user ? (
+                            <>
+                                <p>👤 {user.username}</p>
+                                <button onClick={logout}>Đăng xuất</button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/signin">Đăng nhập</Link>
+                                <Link to="/signup">Đăng ký</Link>
+                            </>
+                        )}
+                    </div>
+                )} */}
             </ul>
         </nav>
     );
